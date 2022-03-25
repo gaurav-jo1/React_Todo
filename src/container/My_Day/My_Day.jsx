d70bf636af31fc28ed74bfea8241e242401fe67f
Gaurav-jo1 <gj451026@gmail.com>
2022-03-25 22:41:16 +0530

    add div to input tags

--- src/container/My_Day/My_Day.jsx
+++ src/container/My_Day/My_Day.jsx
@@ -9,8 +9,12 @@ const My_Day = () => {
       <h1>My Day</h1>
 
 
-      <input className='Input_Add'  type="text" name="myDay_task" placeholder="Try typing 'Exercise by 5 PM' "   />
-      <input className='Input_Submit'  type="submit" value="+" />
+      <div className='adding_task_div' >
+        <input className='Input_Add' type="text" name='myDay_task' placeholder="Try typing 'Exercise by 5 PM' "  />
+        <input className='Input_Submit' type="submit"  value="+"/>
+      </div>   
+      {/* <input className='Input_Add'  type="text" name="myDay_task" placeholder="Try typing 'Exercise by 5 PM' "   />
+      <input className='Input_Submit'  type="submit" value="Submit"  placeholder='+'/> */}
 
     </div>
   )
