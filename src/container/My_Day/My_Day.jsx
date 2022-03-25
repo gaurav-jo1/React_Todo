c25ccbb6f547094d67724e6a4a1a1774d22f3d00
Gaurav-jo1 <gj451026@gmail.com>
2022-03-25 18:39:27 +0530

    Basic SCSS and strucure is set up

--- src/container/My_Day/My_Day.jsx
+++ src/container/My_Day/My_Day.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import './My_Day.scss';
+import {AiOutlineUpCircle} from 'react-icons/ai';
 
 const My_Day = () => {
   return (
 
-    <div>
-        <p>My_Day</p>
+    <div className='main_Background'>
+      <h1>My Day</h1>
+
+
+      <input className='Input_Add'  type="text" name="myDay_task" placeholder="Try typing 'Exercise by 5 PM' "   />
+      <input className='Input_Submit'  type="submit" value="+" />
+
     </div>
   )
 }
--- src/container/Side_Bar.jsx
+++ src/container/Side_Bar.jsx
@@ -1,13 +1,29 @@
 import React from 'react';
 import './Side_Bar.scss';
 import User from '../assets/user.png';
+import {BsFillSunFill} from 'react-icons/bs';
+import {VscStarFull} from 'react-icons/vsc';
+import {RiFileListFill} from 'react-icons/ri';
+import {FaRegHandPointRight} from 'react-icons/fa';
+import {MdOutlineStarOutline} from 'react-icons/md';
+
 
 const Side_Bar = () => {
   return (
     <nav>
-      <div>
+      <div className='UserName' > 
         <img src={User} alt="User_Img" />
+        <h2>Gaurav Joshi</h2>
       </div>
+      <ul>
+          <li> <BsFillSunFill/> &nbsp; My Day</li>
+          <li> <MdOutlineStarOutline  style={{color:'gold'}} /> &nbsp; Important</li>
+          <li> <RiFileListFill style={{color:'skyblue'}} /> &nbsp; Planned</li>
+      </ul>
+      <div className='one_line'/>
+      <ul>
+          <li> <FaRegHandPointRight style={{color:'yellow'}} /> &nbsp; Getting Started</li>
+      </ul>
     </nav>
   )
 }
